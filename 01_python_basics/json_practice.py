6c246835db859d9aef07d3cb33e0f59ed789c546
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-09 23:28:41 +0530

    created valid json

---
+++ 01_python_basics/json_practice.py
@@ -0,0 +1,23 @@
+import json
+
+people_string = '''
+{
+    "people": [
+        {
+          "name": "Sree",
+          "phone":123456789,
+          "email":"sree@gmail.com",
+          "has_license": true 
+        }, 
+        {
+            "name": "Chicku",
+            "phone":345261780,
+            "email": null,
+            "has_license": false
+        }
+    ]
+}
+'''
+
+data = json.loads(people_string)
+print(data) 
