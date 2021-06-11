8a79fad28a865df762f110f67e27d2cfd5548b6b
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-11 23:49:33 +0530

    new practice file

---
+++ 01_python_basics/json_practice2.py
@@ -0,0 +1,10 @@
+'''This documentation involves importing a json file and converting it into a python object'''
+
+import json
+
+with open('country_by_capital.json') as f:
+    data = json.load(f)
+    
+for country in data:
+    print(country)
+    
