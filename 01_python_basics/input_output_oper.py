eb2cc2dd4b727289d977fdc0eaaf28f398ae07a8
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-12 14:04:39 +0530

    accessing individual lines and words

--- 01_python_basics/input_output_oper.py
+++ 01_python_basics/input_output_oper.py
@@ -7,9 +7,24 @@ print(myfile.read())
 
 # readlines() method
 myfile.seek(0)
-print(myfile.readlines())
+content = myfile.readlines()
+print(content)
 
-# printing out each line 
-myfile.seek(0)
-for line in myfile.readlines():
+# printing out each line
+
+for line in content:
     print(line)
+
+# accessing each line and each word in a particular line
+
+#accessing first line
+print(content[0])
+
+#accessing first word of first line
+print(content[0][0:4])
+
+#accessing second line
+print(content[1])
+
+#accessing the word 'purpose' in second line
+print(content[1][9:16])
