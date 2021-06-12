ffd22db77e2d204678e656264109feac5fcf1c4a
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-12 13:50:58 +0530

    readlines() method

--- 01_python_basics/input_output_oper.py
+++ 01_python_basics/input_output_oper.py
@@ -2,5 +2,14 @@
 
 myfile = open('/home/user/Verified/01_python_basics/test.txt')
 
-#read() method
+# read() method
 print(myfile.read())
+
+# readlines() method
+myfile.seek(0)
+print(myfile.readlines())
+
+# printing out each line 
+myfile.seek(0)
+for line in myfile.readlines():
+    print(line)
