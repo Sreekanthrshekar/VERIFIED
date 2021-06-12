acb73581d5e006495debd1bf7205dfc34bbdad66
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-12 13:45:24 +0530

    read() method

---
+++ 01_python_basics/input_output_oper.py
@@ -0,0 +1,6 @@
+'''This documentation is for demonstating vaious I/O methods for txt files'''
+
+myfile = open('/home/user/Verified/01_python_basics/test.txt')
+
+#read() method
+print(myfile.read())
