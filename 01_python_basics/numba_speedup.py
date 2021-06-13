694a985af8d9db42be003b339c96beaae10f9a38
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-13 19:49:48 +0530

    formatted as per pylint

--- 01_python_basics/numba_speedup.py
+++ 01_python_basics/numba_speedup.py
@@ -1,24 +1,29 @@
-from numba import jit
+'''This documentation demonstrates the impact of jit from numba '''
+
 import time
+from numba import jit
+
 
 # without jit
 
-def power(x,y):
-    return x**y
+def power(num_1,num_2):
+    '''This function takes in two arguments num_1 and num_2 and returns the output num_1^num_2'''
+    return num_1**num_2
 
 start = time.time()
-power(3000000,2000000)
+power(30000000,20000000)
 end = time.time()
-print(end-start)
+print(end-start) #551 seconds (9+ mins)
 
 
 #with jit
 
 @jit
-def power1(x,y):
-    return x**y
+def power1(num_1,num_2):
+    '''This function takes in two arguments num_1 and num_2 and returns the output num_1^num_2'''
+    return num_1**num_2
 
 start = time.time()
-power1(3000000,2000000)
+power1(30000000,20000000)
 end = time.time()
-print(end-start)
+print(end-start) # 0.326 seconds
