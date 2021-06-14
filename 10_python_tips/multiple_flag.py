3a6dcb7e34a2fed406ff6aa0c15f6b9037307fc7
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-14 21:38:02 +0530

    different ways to check flags

--- 10_python_tips/multiple_flag.py
+++ 10_python_tips/multiple_flag.py
@@ -1,2 +1,9 @@
 ''' Different ways to test multiple flags in Python '''
 NUM_1, NUM_2, NUM_3 = 0,1,0
+
+if NUM_1 ==1 or NUM_2 == 1 or NUM_3 ==1:
+    print('passed')
+
+
+if 1 in (NUM_1,NUM_2,NUM_3):
+    print('passed')
