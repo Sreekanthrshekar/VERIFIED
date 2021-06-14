2ba4e00e956b59171c548ef3458c93a3700cbe55
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-14 21:44:08 +0530

    formatted using pylint

--- 10_python_tips/multiple_flag.py
+++ 10_python_tips/multiple_flag.py
@@ -8,10 +8,9 @@ if NUM_1 ==1 or NUM_2 == 1 or NUM_3 ==1:
 if 1 in (NUM_1,NUM_2,NUM_3):
     print('passed')
 
-# boolean condition
+# boolean condition (2 ways)
 if NUM_1 or NUM_2 or NUM_3:
     print('passed')
 
 if any((NUM_1,NUM_2,NUM_3)):
     print('passed')
-
