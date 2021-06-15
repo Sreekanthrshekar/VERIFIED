e0fa7a1563d6a0464f4694507b6cf3f690046748
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-15 14:40:48 +0530

    module for arithmatic operations between two numbers

---
+++ 09_python_inter/arith_oper.py
@@ -0,0 +1,23 @@
+''' This document demonstrates various arithmatic operations between two numbers '''
+
+    
+def add(num_1,num_2):
+    ''' Add function '''
+    return num_1 + num_2
+
+def subtract(num_1,num_2):
+    ''' Subtract function '''
+    return num_1 - num_2
+
+def multiply(num_1,num_2):
+    ''' Multiply function '''
+    return num_1 * num_2
+
+def divide(num_1,num_2):
+    ''' Divide function '''
+    if num_2 == 0:
+        raise ValueError('cannot divide by zero')
+    return num_1 / num_2
+
+
+
