b82e1510531c9b4cd87111493e83cb10848c2ec6
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-15 13:58:37 +0530

    formatted using pylint

--- 09_python_inter/context_manager_classes_and_functions.py
+++ 09_python_inter/context_manager_classes_and_functions.py
@@ -1,4 +1,4 @@
-'''This document demonstrates creating custom classes and functions 
+'''This document demonstrates creating custom classes and functions
 which has the functionality of 'with' '''
 
 from contextlib import contextmanager
@@ -6,15 +6,17 @@ from contextlib import contextmanager
 # custom class having 'with' functionality
 
 class ManagedFile():
+    '''This class has 'with' functionality '''
 
     def __init__(self,name,mode):
         self.name = name
         self.mode = mode
-    
+        self.file = None
+
     def __enter__(self):
         self.file = open(self.name,self.mode)
         return self.file
-    
+
     def __exit__(self, exc_type, exc_value, exc_tb):
         if self.file:
             self.file.close()
@@ -23,32 +25,26 @@ class ManagedFile():
 
 @contextmanager
 def managed_file(name, mode):
+    ''' This function has 'with' functionality '''
 
     try:
-        f = open(name,mode)
-        yield f
+        fil = open(name,mode)
+        yield fil
 
     finally:
-        f.close()
-
-
+        fil.close()
 
 
 if __name__ == '__main__':
- 
- # testing custom class 
-    
-    with ManagedFile('hello.txt','w') as f:
-        f.write('hello, world!\n')
-        f.write('ok,bye!\n')
+
+ # testing custom class
+
+    with ManagedFile('hello.txt','w') as file_name:
+        file_name.write('hello, world!\n')
+        file_name.write('ok,bye!\n')
 
 # testing custom function
-   
+
     with managed_file('hello.txt','w') as f:
         f.write('This is an overwritten sentence.\n')
         f.write('Thanks again..\n')
-
-
-        
-
-
