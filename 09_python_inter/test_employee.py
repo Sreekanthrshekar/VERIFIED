7290caa5f56002c24aec65e9e6f9a8d5e6d9cfd1
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-16 23:50:41 +0530

    formatted using pylint

--- 09_python_inter/test_employee.py
+++ 09_python_inter/test_employee.py
@@ -7,43 +7,43 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
     ''' This class tests the methods in Employee class '''
-    
+
     emp_1 = Employee('John','Smith', 95000.0)
     emp_2 = Employee('Sreekanth', 'Raja', 185000.0)
-    
+
     def test_email(self):
         ''' Tests the email function '''
         self.assertEqual(self.emp_1.email,'John.Smith@gmail.com')
         self.assertEqual(self.emp_2.email,'Sreekanth.Raja@gmail.com')
-        
+
         self.emp_1.first_name = 'Jane'
         self.emp_2.first_name = 'Gopal'
-        
+
         self.assertEqual(self.emp_1.email,'Jane.Smith@gmail.com')
         self.assertEqual(self.emp_2.email,'Gopal.Raja@gmail.com')
 
     def test_fullname(self):
         ''' Tests the full name function '''
-        
+
         self.emp_1.first_name = 'John'
         self.emp_2.first_name = 'Sreekanth'
-        
+
         self.assertEqual(self.emp_1.fullname,'John Smith')
         self.assertEqual(self.emp_2.fullname,'Sreekanth Raja')
-        
+
         self.emp_1.first_name = 'Jane'
         self.emp_2.first_name = 'Gopal'
-        
+
         self.assertEqual(self.emp_1.fullname,'Jane Smith')
         self.assertEqual(self.emp_2.fullname,'Gopal Raja')
-        
+
     def test_apply_raise(self):
         ''' Test apply_raise function '''
-                
+
         self.emp_1.first_name = 'John'
         self.emp_2.first_name = 'Sreekanth'
-        
-                
+
+
         self.assertEqual(self.emp_1.apply_raise(), 99750.0)
         self.assertEqual(self.emp_2.apply_raise(), 194250.0)
 
