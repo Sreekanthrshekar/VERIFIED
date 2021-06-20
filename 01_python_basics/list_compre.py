4e0dd2c47b99b00899fc8212d07bd055b310557d
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-20 17:00:38 +0530

    formatted using pylint

--- 01_python_basics/list_compre.py
+++ 01_python_basics/list_compre.py
@@ -1,4 +1,4 @@
-''' This document demonstrates list comprehension ''' 
+''' This document demonstrates list comprehension '''
 
 nums = [1,2,3,4,5,6,7,8,9]
 
@@ -56,7 +56,7 @@ print(nums_even_1)
 # step 1: write the first sentence of for loop inside a list
 # nums_even_2 = [for n in nums]
 
-# step 2: add the condition 
+# step 2: add the condition
 # nums_even_2 = [for n in nums if n%2 == 0]
 
 # step 3: add in front of that, the result we want (here n)
@@ -79,4 +79,3 @@ print(my_list_1)
 # Method 2 -  list comprehension
 my_list_2 = [(letter, num) for letter in 'abcd' for num in '0123']
 print(my_list_2)
-
