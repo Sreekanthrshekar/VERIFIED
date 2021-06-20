251ac9b786a7b13db53faa4ccbc65a8a7617f6e1
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-20 17:10:39 +0530

    simple example of dict comprehension

--- 01_python_basics/dict_compre.py
+++ 01_python_basics/dict_compre.py
@@ -1,2 +1,34 @@
 ''' This document demonstrates dictionary comprehension ''' 
 
+names = [
+    'Bruce',
+    'Clark',
+    'Peter',
+    'Logan',
+    'Wade',
+    ]
+
+heros = [
+    'Batman',
+    'Superman',
+    'Spiderman',
+    'Wolverine',
+    'Deadpool',
+    ]
+
+# Create a dictionary with name names,heros as key - value pair
+
+
+# Method 1 : usual method
+
+my_dict_1 ={}
+
+for name, hero in zip(names, heros):
+    my_dict_1[name] = hero
+
+print(my_dict_1)
+
+# Method 2: dict comprehension
+
+my_dict_2 = {name:hero for name,hero in zip(names,heros)}
+print(my_dict_2)
