1ab58fca31658a6e4c710ee25313bcaf7c4a9e10
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-21 19:49:58 +0530

    formatted using pylint

--- 09_python_inter/logging_basics.py
+++ 09_python_inter/logging_basics.py
@@ -1,6 +1,6 @@
 ''' This document demonstrates logging basics '''
 
-import logging as logging
+import logging
 
 # FIVE LOGGING LEVELS:
 
@@ -8,22 +8,24 @@ import logging as logging
 
 # INFO: Confirmation that things are working as expected.
 
-# WARNING:    An indication that something unexpected happened, 
+# WARNING:    An indication that something unexpected happened,
             # or indicative of some problem in near future.
             # (e.g., 'disk space low')
             # The software is still working as expected.
 
-# ERROR:   Due to a more serious problem, 
+# ERROR:   Due to a more serious problem,
          # the software has not been able to perform some function.
 
 # CRITICAL:   A serious error, indicating that the program itself may be
             # unable to continue running.
 
 
-# LOG RECORD ATTRIBUTES - https://docs.python.org/3/library/logging.html?highlight=logrecord%20attributes
+# LOG RECORD ATTRIBUTES -
+# https://docs.python.org/3/library/logging.html?highlight=logrecord%20attributes
 
 
-logging.basicConfig(filename = './09_python_inter/arith.log',level=logging.DEBUG,format='%(levelname)s:%(message)s')
+logging.basicConfig(filename = './09_python_inter/arith.log',level=logging.DEBUG,
+                    format='%(levelname)s:%(message)s')
 
 def add(num_1,num_2):
     ''' Add function '''
@@ -46,16 +48,14 @@ def divide(num_1,num_2):
 NUM_1 =100
 NUM_2 =20
 
-add_result = add(NUM_1,NUM_2)
-logging.debug(f"Add: {NUM_1} + {NUM_2} = {add_result}")
+ADD_RESULT = add(NUM_1,NUM_2)
+logging.debug(f"Add: {NUM_1} + {NUM_2} = {ADD_RESULT}")
 
-sub_result = subtract(NUM_1,NUM_2)
-logging.debug(f"Subtract: {NUM_1} - {NUM_2} = {sub_result}")
+SUB_RESULT = subtract(NUM_1,NUM_2)
+logging.debug(f"Subtract: {NUM_1} - {NUM_2} = {SUB_RESULT}")
 
-mul_result = multiply(NUM_1,NUM_2)
-logging.debug(f"Multiply: {NUM_1} * {NUM_2} = {mul_result}")
+MUL_RESULT = multiply(NUM_1,NUM_2)
+logging.debug(f"Multiply: {NUM_1} * {NUM_2} = {MUL_RESULT}")
 
-div_result = divide(NUM_1,NUM_2)
-logging.debug(f"Divide: {NUM_1} / {NUM_2} = {div_result}")
-
-
+MUL_RESULT = divide(NUM_1,NUM_2)
+logging.debug(f"Divide: {NUM_1} / {NUM_2} = {MUL_RESULT}")
