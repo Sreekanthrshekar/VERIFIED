b8531159eed62853ac7ff287751da5618cc8097e
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-23 14:36:44 +0530

    added a class vaiable

--- 09_python_inter/employee.py
+++ 09_python_inter/employee.py
@@ -10,14 +10,18 @@ class Employee():
     ''' Employee Class: 
     has methods to get email,fullname and apply salary raise '''
     
+    # Class variables
     raise_amount = 1.05 # 5% increase
+    count_of_employees = 0
     
     def __init__(self,first_name,last_name,salary):
         self.first_name = first_name
         self.last_name = last_name
         self.salary = salary
+        Employee.count_of_employees += 1
         
         logging.info('Created employee: {} {}'.format(self.first_name, self.last_name))
+        logging.info('Current count of employees: {}'.format(Employee.count_of_employees))
 
     
     @property
@@ -39,4 +43,9 @@ class Employee():
 
 if __name__ == '__main__':
     emp_1 = Employee('John','Smith', 95000)
+    emp_2 = Employee('Raja','Gopal', 198000)
+
+    print(Employee.raise_amount)
+    print(emp_1.raise_amount)
+    print(emp_2.raise_amount)
 
