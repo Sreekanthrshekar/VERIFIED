f27cdc3a0afbcf04a7a9483373bfaeb2854b10ad
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-23 21:54:32 +0530

    added description for first class functions

---
+++ 09_python_inter/first_class_functions.py
@@ -0,0 +1,12 @@
+''' This document illustrates first class functions of python '''
+
+''' First class functions:
+A programming language is said to have first-class functions
+if it treats functions as first-class citizens 
+
+First-class citizens (programming):
+A first class citizen (sometimes called first-clas objects) 
+in a programming language is an entity which supports all the operations
+generally available to other entities. These operations typically include
+being passed as an argument, returning from a function,
+and assigned to a variable.'''
