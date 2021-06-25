bba2cfdd237beeb10ce7f346988f3e84187ce756
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-25 10:21:13 +0530

    documentation of closure

---
+++ 09_python_inter/closure.py
@@ -0,0 +1,9 @@
+'''CLOSURE
+Wikipedia  says, A closure is a recorsd storing a function together with
+an environment: a mapping associating each free variable of the function 
+with the value or storage location to which the name was bound when the
+closure was created.
+
+A closure, unlike a plain function, allows the function to access those 
+captured variables through the closure's reference to them, even when
+the function is invoked outside their scope'''
