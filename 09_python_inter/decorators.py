1d72f8ca7d18cf02d717778e9a67f4ac09682f3b
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-26 16:19:06 +0530

    added a simple custom decorator function

--- 09_python_inter/decorators.py
+++ 09_python_inter/decorators.py
@@ -6,16 +6,23 @@ all of this, without altering the source code of the original function
 that was passed in '''
 
 
+# an example of closure
 
-def outer_func2(msg):
+def outer_func(msg):
     ''' outer function '''
-    def inner_func2():
+    def inner_func():
         ''' inner function '''
         print(msg)
+    return inner_func
 
-    return inner_func2
-
-
-wow_func = outer_func2('Wow')
-
+wow_func = outer_func('Wow')
 wow_func()
+
+# a simple custom decorator message
+
+def decorator_func(original_func):
+    ''' decorator function '''
+    def wrapper_func():
+        ''' wrapper function '''
+        return original_func
+    return wrapper_func
