07dded7a641b19d685d0f3622e3d43cc177d0f7a
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-06-26 15:35:15 +0530

    example 1 of closure

--- 09_python_inter/closure.py
+++ 09_python_inter/closure.py
@@ -7,3 +7,16 @@ closure was created.
 A closure, unlike a plain function, allows the function to access those 
 captured variables through the closure's reference to them, even when
 the function is invoked outside their scope'''
+
+# Example 1
+
+def outer_func():
+    message = 'Hello'
+    
+    def inner_func():
+        print(message)
+        
+    return inner_func()
+
+
+outer_func()
