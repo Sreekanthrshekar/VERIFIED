add65955c3f20dbede13344277dbc75a00154336
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-06 12:47:23 +0530

    accessing objects using identifiers

--- 09_python_inter/namedtuple.py
+++ 09_python_inter/namedtuple.py
@@ -4,5 +4,21 @@ from collections import namedtuple
 
 # Defining 'Car' datatype having 3 fields : color, milage and fueltype
 
-Car = namedtuple('Car','color milage fueltype') 
+Car = namedtuple('Car','color milage fueltype')
 # 'color milage fueltype' is same as ['color', 'milage', 'fueltype']
+
+my_car = Car('red','5555.56', 'petrol')
+
+# accessing objects:
+    # standard way: using index
+print(my_car[0])
+print(my_car[1])
+print(my_car[2])
+    # using unique identifiers
+print(my_car.color)
+print(my_car.milage)
+print(my_car.fueltype)
+
+# create ordinary tuple from namedtuple
+tup = tuple(my_car)
+print(tup)
