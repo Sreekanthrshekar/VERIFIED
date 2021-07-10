4caf5340e9827a06bb72aafb90cac56f1100b186
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-10 23:37:14 +0530

    capture the output in a variable

--- 09_python_inter/subprocess_methods.py
+++ 09_python_inter/subprocess_methods.py
@@ -10,3 +10,9 @@ print('''
 
 # adding additional arguments
 subprocess.run(['ls','-la'], check=True)
+print('''
+     ''')
+
+# capture the output in a variable
+list_dir = subprocess.run(['ls','-la'], check=True,capture_output=True)
+print(list_dir.stdout) #output is captured in bytes
