a52e2277864e41f5e1eb6e151727193c01e80de7
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-10 23:29:11 +0530

    adding additional arguments

--- 09_python_inter/subprocess_methods.py
+++ 09_python_inter/subprocess_methods.py
@@ -1,4 +1,12 @@
 ''' This document deals with subprocess methods'''
 import subprocess
 
+print('''
+     ''')
+
 subprocess.run('ls', check=True)
+print('''
+     ''')
+
+# adding additional arguments
+subprocess.run(['ls','-la'], check=True)
