eb1a34f75c2f83af985eed5777c700b1463bc8f0
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-11 19:50:35 +0530

    alternative to capture_output

--- 09_python_inter/subprocess_methods.py
+++ 09_python_inter/subprocess_methods.py
@@ -29,8 +29,18 @@ print('''
 
 # decoding the output from bytes: Method 2
 list_dir = subprocess.run(['ls','-la'], check=True,capture_output=True,text=True)
-print(list_dir.stdout)
+print('list_dir: \n', list_dir.stdout)
 
+print('''
+      
+      ''')
+# alternative to capture_output
+list_dir1 = subprocess.run('ls -la', check=True,stdout=subprocess.PIPE, shell=True,text=True)
+print('list_dir1:\n', list_dir1.stdout)
+
+print('''
+      
+      ''')
 # redirecting stdout to a file
 with open('./09_python_inter/output.txt', 'w') as f:
     subprocess.run(['ls','-la'], check=True, stdout=f,text=True)
