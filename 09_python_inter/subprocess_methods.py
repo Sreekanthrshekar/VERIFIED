b5f0e33641485b5845c27dde9ca298270da9a538
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-11 19:44:29 +0530

    redirecting stdout to a file

--- 09_python_inter/subprocess_methods.py
+++ 09_python_inter/subprocess_methods.py
@@ -30,3 +30,7 @@ print('''
 # decoding the output from bytes: Method 2
 list_dir = subprocess.run(['ls','-la'], check=True,capture_output=True,text=True)
 print(list_dir.stdout)
+
+# redirecting stdout to a file
+with open('./09_python_inter/output.txt', 'w') as f:
+    subprocess.run(['ls','-la'], check=True, stdout=f,text=True)
