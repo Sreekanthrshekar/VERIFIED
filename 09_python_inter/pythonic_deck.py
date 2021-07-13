3bc1c6e0c198641fadde05474344a440a92eea59
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-13 20:41:41 +0530

    created a class named FrenchDeck

---
+++ 09_python_inter/pythonic_deck.py
@@ -0,0 +1,22 @@
+''' This document demonstrates the power of implementing two
+dunder methods: __len__ and __getitem__ '''
+
+from collections import namedtuple
+
+Card = namedtuple('Card','rank suit')
+
+class FrenchDeck:
+    ''' has methods for len and getitem '''
+
+    ranks = [str(n) for n in range(2,11)] + list('JKQA')
+    suits = 'spades clubs diamonds hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank,suit) for rank in self.ranks
+                       for suit in self.suits]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
