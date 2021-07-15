df240a0ef4e1b03d82814480f7b74be82819e2fc
Sreekanth R Shekar <sreekanthrshekar@gmail.com>
2021-07-15 19:33:39 +0530

    sorting list in ascending order

---
+++ 01_python_basics/sorting_all.py
@@ -0,0 +1,14 @@
+''' This document deals with sorting lists, tuples and objects '''
+
+li =[2,5,7,3,1,9,4,6]
+# Sort the list in ascending order
+# method 1
+sorted_li = sorted(li)
+print('sorted list:\n', sorted_li)
+print('')
+print('original list:\n', li)
+print('')
+# method 2
+li.sort()
+print('sorted list:\n', li )
+print('')
